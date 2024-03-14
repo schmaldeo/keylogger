dd4913ca7f4473cc45f5d3b5ea936d515acd5463
schmatteo <michal.skrzyszowski@hotmail.com>
2024-03-14 15:58:23 +0100

    Change all \n's to Environment.NewLine's

--- src/LogFile.cs
+++ src/LogFile.cs
@@ -28,16 +28,16 @@ public class LogFile : ILogger
         if (new FileInfo(PathToFile).Length != 0) return;
 
         Writer.Write(
-            $"https://github.com/schmaldeo/keylogger v{Assembly.GetEntryAssembly()!.GetName().Version} {DateTime.Now}\n\n" +
+            $"https://github.com/schmaldeo/keylogger v{Assembly.GetEntryAssembly()!.GetName().Version} {DateTime.Now}{Environment.NewLine}{Environment.NewLine}" +
             $"If you see something like: [CODE: xxxx], you can check what key the code represents on " +
-            $"https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes\n\n" +
+            $"https://learn.microsoft.com/en-us/windows/win32/inputdev/virtual-key-codes{Environment.NewLine}{Environment.NewLine}" +
             $"Keyboard layout: {Keyboard.GetKeyboardLayout()}. Find more information about it on: " +
-            $"https://winprotocoldoc.blob.core.windows.net/productionwindowsarchives/MS-LCID/%5bMS-LCID%5d.pdf\n" +
+            $"https://winprotocoldoc.blob.core.windows.net/productionwindowsarchives/MS-LCID/%5bMS-LCID%5d.pdf{Environment.NewLine}" +
             $"The modifier keys only modify the output to this file for the following characters: " +
             $"; , . / ` [ ] \\ ', for the other ones like shift + numbers or alt graph + letters, you " +
             $"need to find out yourself what keyboard layout it is and then just find out yourself what " +
             $"the output was, as you can clearly see when the modifier keys are pressed and released, " +
-            $"as well as what was typed in while those were active.\n\n");
+            $"as well as what was typed in while those were active.{Environment.NewLine}{Environment.NewLine}");
         Writer.Flush();
     }
 }
